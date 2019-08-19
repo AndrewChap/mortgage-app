471f1c6bed684173d178920d45d9b59662b8ac33
AndrewChap <andrew@andrewchap.com>
2019-08-18 20:41:14 -0600

    Plotting 'total amount spent on house' as default

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import mortgagetvm as mort
+import pdb
+db = pdb.set_trace
 server = Flask(__name__)
 
 num_mortgages = 2
@@ -18,18 +20,36 @@ dashApp = dash.Dash(
     routes_pathname_prefix='/dash/'
 )
 dashApp.scripts.config.serve_locally = True
+globalHtmls = []
+globalHtmls.append(
+    html.Div([
+        html.Label('Home Cost')
+    ])
+)
 mortHtmls = []
 for i in range(num_mortgages):
-    mortHtmls += [html.Label('Mortgage {}'.format(i)),
-                  dcc.Input(id='mir-state{}'.format(i), value='3.5', type='text'),
-                  html.Div(id='mir-div{}'.format(i), className='field-div')]
-
+    mortHtmls.append(
+        html.Div([
+            html.Label('Mortgage {}'.format(i)),
+            html.Div([
+                html.Label('Mortgage Rate'.format(i)),
+                dcc.Input(id='mir-state{}'.format(i), value='3.5', type='text'),
+                html.Div(id='mir-div{}'.format(i), className='field-div')
+            ]),
+            html.Div([
+                html.Label('Down Payment'.format(i)),
+                dcc.Input(id='mdp-state{}'.format(i), value='0.2', type='text'),
+                html.Div(id='mdp-div{}'.format(i), className='field-div')
+            ]),
+        ])
+    )
 dashApp.layout = html.Div(children=[
     html.H1(children='Time-value Loan & Investment Analyzer',
         style={
             'textAlign' : 'center',
         }
     ),
+    *globalHtmls,
     *mortHtmls,
     html.Button(id='mir-button', n_clicks = 0, children='Calculate!'),
     dcc.Graph(
@@ -37,30 +57,33 @@ dashApp.layout = html.Div(children=[
     ),
 ])
 
-@dashApp.callback(
-    [Output('mir-div{}'.format(i), 'children') for i in range(num_mortgages)],
-    [Input('mir-button', 'n_clicks')],
-    [State('mir-state{}'.format(i), 'value') for i in range(num_mortgages)],
-)
-def update_output_div(button, *input_values):
-    input_floats = [float(input_value) for input_value in input_values]
-    return ['Rate = {:.2f}%, x = {}'.format(input_float,mortgage.netWorth.data[-1]) for input_float,mortgage in zip(input_floats,mortgages)]
+#@dashApp.callback(
+#    [Output('mir-div{}'.format(i), 'children') for i in range(num_mortgages)],
+#    [Input('mir-button', 'n_clicks')],
+#    [State('mir-state{}'.format(i), 'value') for i in range(num_mortgages)],
+#)
+#def update_output_div(button, *input_values):
+#    input_floats = [float(input_value) for input_value in input_values]
+#    return ['Rate = {:.2f}%, x = {}'.format(input_float,mortgage.netWorth.data[-1]) for input_float,mortgage in zip(input_floats,mortgages)]
 
 @dashApp.callback(
     Output('main-plot', 'figure'),
     [Input('mir-button', 'n_clicks')],
-    [State('mir-state{}'.format(i), 'value') for i in range(num_mortgages)],
+    [State('mir-state{}'.format(i), 'value') for i in range(num_mortgages)] + 
+    [State('mdp-state{}'.format(i), 'value') for i in range(num_mortgages)], 
 )
 def update_figure(button, *input_values):
     input_floats = [float(input_value) for input_value in input_values]
     for i in range(num_mortgages):
+        j = 2*i
         mortgages[i] = mort.Mortgage(mortgageRate=input_values[i])
+        mortgages[i] = mort.Mortgage(downPayment=input_values[i+num_mortgages])
         mortgages[i].simulateMortgage()
-    data = [{'x': mortgage.timeVector.data, 'y': mortgage.netWorth.data} for mortgage in mortgages]
+    data = [{'x': mortgage.timeVector.data, 'y': mortgage.totalAmountSpent.data} for mortgage in mortgages]
     return {
         'data': data,
         'layout': {
-            'title': 'Dash Data Visualization'
+            #'title': 'Dash Data Visualization'
         }
     }
 
